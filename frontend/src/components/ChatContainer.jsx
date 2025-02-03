a85eaf24c8169424664bf9c8faa5a77e5c4d57b2
VinayKumar7549 <130210733+VinayKumar7549@users.noreply.github.com>
2025-02-03 14:13:35 +0530

    ChatHeader and MessageSkeleton added

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import ChatHeader from './ChatHeader';
+import MessageInput from './MessageInput';
+import MessageSkeleton from './LoadingSkeleton/MessageSkeleton';
 
 const ChatContainer = () => {
+
+  const{ messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);  //we are using id bcz we the getMessages is expecting a userId
+  }, [selectedUser._id, getMessages]); //we are passing selectedUser._id as dependency so that whenever selectedUser changes we will get the messages of the new selected user as the getMessages is called again
+
+  if(isMessagesLoading) return(
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+      <MessageSkeleton />
+      <MessageInput />
+    </div>
+  )
+
   return (
-    <div>ChatContainer</div>
+    <div className='flex-1 flex flex-col overflow-auto'>
+      <ChatHeader />
+
+      <p>messagews</p>
+
+      <MessageInput/>
+
+    </div>
   )
 }
 
