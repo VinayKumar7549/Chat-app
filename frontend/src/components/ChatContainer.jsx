15e555638843a1f43b86c82f5d34f1123bf7cfb3
VinayKumar7549 <130210733+VinayKumar7549@users.noreply.github.com>
2025-03-09 02:09:22 +0530

    Completed the MessageInput

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -9,16 +9,17 @@ const ChatContainer = () => {
   const{ messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
 
   useEffect(() => {
-    getMessages(selectedUser._id);  //we are using id bcz we the getMessages is expecting a userId
+    getMessages(selectedUser._id);  //we are using id bcz getMessages is expecting a userId
   }, [selectedUser._id, getMessages]); //we are passing selectedUser._id as dependency so that whenever selectedUser changes we will get the messages of the new selected user as the getMessages is called again
 
-  if(isMessagesLoading) return(
+  if(isMessagesLoading) {
+    return(
     <div className='flex-1 flex flex-col overflow-auto'>
       <ChatHeader />
       <MessageSkeleton />
       <MessageInput />
     </div>
-  )
+  )}
 
   return (
     <div className='flex-1 flex flex-col overflow-auto'>
