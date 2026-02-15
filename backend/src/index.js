a8df1269eb28aeed1925df7078b713536e43c779
VinayKumar7549 <sayinivinaykumar7549@gmail.com>
2026-02-15 16:32:25 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,16 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import { connectDB } from "./lib/db.js"         // For mongoDB connection
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import {app, server} from "./lib/socket.js"
 
 dotenv.config();             // Function used for activating dotenv
+
+const __dirname = path.resolve();  // To get the current directory path
 
 const PORT = process.env.PORT;  // To get the port from the .env file
 
@@ -21,6 +25,14 @@ app.use(cors({              // This is used to enable cors in the application
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on port:" + PORT);
